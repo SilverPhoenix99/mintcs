54732e2ca332e01bb9b3d30ea7dae21545e4aa67
Silver Phoenix <silver.phoenix99@gmail.com>
2016-01-07 11:37:32 +0000

    Added automatic creation of gen directory in batch scripts.

--- GenLexer.bat
+++ GenLexer.bat
@@ -1,10 +1,12 @@
 @echo off
 
-set MINT_CUR_DIR=%~dp0
+set MINT_GEN_DIR=%~dp0
 
-ragel -A -L -G0 "%MINT_CUR_DIR%Compiler\Lexer_exec.csrl" -o "%MINT_CUR_DIR%gen\Lexer_exec.cs"
-ragel -A -L -G0 "%MINT_CUR_DIR%Compiler\Lexer_data.csrl" -o "%MINT_CUR_DIR%gen\Lexer_data.cs"
+if not exist "%MINT_GEN_DIR%gen" mkdir "%MINT_GEN_DIR%gen"
+
+ragel -A -L -G0 "%MINT_GEN_DIR%Compiler\Lexer_exec.csrl" -o "%MINT_GEN_DIR%gen\Lexer_exec.cs"
+ragel -A -L -G0 "%MINT_GEN_DIR%Compiler\Lexer_data.csrl" -o "%MINT_GEN_DIR%gen\Lexer_data.cs"
 
 if %errorlevel% neq 0 (pause)
 
-set MINT_CUR_DIR=
+set MINT_GEN_DIR=
