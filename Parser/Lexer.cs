1d285a184a106be667a306bbfa998a2479d4c8b9
Silver Phoenix <silver.phoenix99@gmail.com>
2016-01-30 21:40:05 +0000

    Removed all dependencies from VM from Parser and Lexer.

--- Parser/Lexer.cs
+++ Parser/Lexer.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Mint.Types;
 using static Mint.Parser.Lexer.States;
 using static Mint.Parser.TokenType;
 
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mint")]
+[assembly: AssemblyTitle("Mint")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mint")]
+[assembly: AssemblyProduct("Mint")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
