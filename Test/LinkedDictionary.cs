041ce4fe375bab38ec4090beaf5ef824a370ed7b
Silver Phoenix <silver.phoenix99@gmail.com>
2016-03-22 09:28:57 +0000

    LinkedDictionary: started implementation of methods. Not yet completed.

--- Test/LinkedDictionary.cs
+++ Test/LinkedDictionary.cs
@@ -1,32 +1,71 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test
 {
     public class LinkedDictionary<TKey, TValue> : IDictionary<TKey, TValue>
     {
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        private readonly Dictionary<TKey, LinkedListNode<Tuple<TKey, TValue>>> map;
+        private readonly LinkedList<Tuple<TKey, TValue>> list = new LinkedList<Tuple<TKey, TValue>>();
+
+        public LinkedDictionary()
         {
-            throw new NotImplementedException();
+            map = new Dictionary<TKey, LinkedListNode<Tuple<TKey, TValue>>>();
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        public LinkedDictionary(int capacity)
         {
-            return GetEnumerator();
+            map = new Dictionary<TKey, LinkedListNode<Tuple<TKey, TValue>>>(capacity);
+        }
+
+        public LinkedDictionary(IEqualityComparer<TKey> comparer)
+        {
+            map = new Dictionary<TKey, LinkedListNode<Tuple<TKey, TValue>>>(comparer);
+        }
+
+        public LinkedDictionary(LinkedDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer = null)
+            : this(comparer)
+        {
+            foreach(var item in dictionary)
+            {
+                Add(item);
+            }
+        }
+        
+        public int Count                  => map.Count;
+        public bool IsReadOnly            => false;
+        public ICollection<TKey> Keys     => map.Keys;
+        public ICollection<TValue> Values => new ReadOnlyCollection<TValue>(map.Values.Select(_ => _.Value.Item2).ToList());
+
+        public TValue this[TKey key]
+        {
+            get { return map[key].Value.Item2; }
+            set { Add(key, value); }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() =>
+            list.Select(_ => new KeyValuePair<TKey, TValue>(_.Item1, _.Item2)).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
         }
 
         public void Add(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            Add(item.Key, item.Value);
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            map.Clear();
+            list.Clear();
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
@@ -39,40 +78,29 @@ namespace Test
             throw new NotImplementedException();
         }
 
-        public bool Remove(KeyValuePair<TKey, TValue> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Count { get; }
-        public bool IsReadOnly { get; }
-        public bool ContainsKey(TKey key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Add(TKey key, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
         public bool Remove(TKey key)
         {
             throw new NotImplementedException();
         }
 
-        public bool TryGetValue(TKey key, out TValue value)
+        public bool Remove(KeyValuePair<TKey, TValue> item)
         {
             throw new NotImplementedException();
         }
 
-        public TValue this[TKey key]
-        {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
-        }
+        public bool ContainsKey(TKey key) => map.ContainsKey(key);
 
-        public ICollection<TKey> Keys { get; }
-        public ICollection<TValue> Values { get; }
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            LinkedListNode<Tuple<TKey, TValue>> val;
+            if(map.TryGetValue(key, out val))
+            {
+                value = val.Value.Item2;
+                return true;
+            }
+
+            value = default(TValue);
+            return false;
+        }
     }
 }
