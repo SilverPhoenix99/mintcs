e9a3116989bf580377cd27a2f0a03ccf41b9273a
Silver Phoenix <silver.phoenix99@gmail.com>
2016-04-11 19:01:24 +0100

    Reflector: Generalized Property method. Object: Added Box methods. Symbol: Added AREF and ASET symbol constants. Compiler: Implemented #CompileLabel. Added rules for kDOT (setter methods ending in '=') and kLBRACK2 (aset '[]=') in #CompileAssign.

--- Mint.VM/Types/Object.cs
+++ Mint.VM/Types/Object.cs
@@ -16,17 +16,28 @@ namespace Mint
         public static readonly Class BASIC_OBJECT_CLASS;
 
         public static readonly Class CLASS;
-        
+
         public static iObject Box(string obj) => new String(obj);
+        public static iObject Box(short obj) => new Fixnum(obj);
+        public static iObject Box(int obj) => new Fixnum(obj);
+        public static iObject Box(long obj) => new Fixnum(obj);
+        public static iObject Box(float obj) => new Float(obj);
+        public static iObject Box(double obj) => new Float(obj);
 
         public static iObject Box(bool obj) => obj ? new TrueClass() : (iObject) new FalseClass();
 
-        public static iObject Box(object o)
+        public static iObject Box(object value)
         {
-            if(o is string) return Box((string) o);
-            if(o is bool)   return Box((bool) o);
+            if(value is iObject) return (iObject) value;
+            if(value is string) return Box((string) value);
+            if(value is bool) return Box((bool) value);
+            if(value is short) return Box((short) value);
+            if(value is int) return Box((int) value);
+            if(value is long) return Box((long) value);
+            if(value is float) return Box((float) value);
+            if(value is double) return Box((double) value);
 
-            return (iObject) o;
+            throw new ArgumentError(nameof(value));
         }
 
         public static bool ToBool(iObject obj) => obj != null && !(obj is NilClass) && !(obj is FalseClass);
