794a3350d7142d4b3cdff664891ef7d6121c45a2
Pedro Miranda <pedro.at.miranda@gmail.com>
2016-05-26 03:06:27 +0100

    Defined Array methods and test cases for clear, join, replace, compact, compact!, reverse, reverse!, first, last, uniq, uniq!, &, +, -, *,

--- Mint.VM/Types/Array.cs
+++ Mint.VM/Types/Array.cs
@@ -1,3 +1,4 @@
+using Mint.Reflection.Parameters.Attributes;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,22 @@ namespace Mint
         public Array(IEnumerable<iObject> objs = null) : base(Class.ARRAY)
         {
             list = objs == null ? new List<iObject>() : new List<iObject>(objs);
+        }
+
+        public Array(int count) : this()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(new NilClass());
+            }
+        }
+
+        public Array(int count, iObject obj) : this()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(obj);
+            }
         }
 
         public Array(params iObject[] objs) : this((IEnumerable<iObject>) objs)
@@ -46,37 +63,16 @@ namespace Mint
             }
         }
 
-        public iObject this[iObject index]
-        {
-            get
-            {
-                int i;
-                //if(index is Fixnum) // TODO !(index is Integer)
-                {
-                    i = (int) (Fixnum) index;
-                }
-                return this[i];
-            }
-            set
-            {
-                int i;
-                //if(index is Fixnum) // TODO !(index is Integer)
-                {
-                    i = (int) (Fixnum) index;
-                }
-                this[i] = value;
-            }
-        }
-
         public iObject Push(params iObject[] elements)
         {
             list.AddRange(elements);
             return this;
         }
 
-        internal void Add(iObject element)
+        public Array Add(iObject element)
         {
             list.Add(element);
+            return this;
         }
 
         public IEnumerator<iObject> GetEnumerator() => list.GetEnumerator();
@@ -84,6 +80,114 @@ namespace Mint
         IEnumerator IEnumerable.GetEnumerator() => list.GetEnumerator();
 
         public override string ToString() => $"[{string.Join(", ", list.Select(_ => _.Inspect()))}]";
+
+        public Array AndAlso(Array other)
+        {
+            var result = new Array(list);
+            result.list.RemoveAll(item => !other.list.Contains(item));
+            result.UniqSelf();
+            return result;
+        }
+
+        public iObject First()
+        {
+            return this[0];
+        }
+
+        public iObject Last()
+        {
+            return this[-1];
+        }
+
+        public Array Clear()
+        {
+            list.Clear();
+            return this;
+        }
+
+        public Array CompactSelf()
+        {
+            list.RemoveAll(item => item == null || item.IsA(Class.NIL));
+            return this;
+        }
+
+        public Array Compact() => new Array(list).CompactSelf();
+
+        public string Join([Optional] string str = "")
+        {
+            if(str == null)
+            {
+                str = "";
+            }
+            return string.Join(str, list);
+        }
+
+        public Array Replace(Array other)
+        {
+            if(other == null)
+            {
+                throw new TypeError("no implicit conversion of nil into Array");
+            }
+            list.Clear();
+            list.AddRange(other.list);
+            return this;
+        }
+
+        public Array ReverseSelf()
+        {
+            list.Reverse();
+            return this;
+        }
+
+        public Array Reverse() => new Array(list).ReverseSelf();
+
+        public Array UniqSelf()
+        {
+            var buffer = list.Distinct<iObject>().ToList();
+            list.Clear();
+            list.AddRange(buffer);
+            return this;
+        }
+
+        public Array Uniq() => new Array(list).UniqSelf();
+
+        public static Array operator +(Array left, Array right)
+        {
+            if(left == null || right == null)
+            {
+                throw new TypeError("no implicit conversion of nil into Array");
+            }
+            var result = new Array(left.list);
+            result.list.AddRange(right);
+            return result;
+        }
+
+        public static Array operator *(Array left, Fixnum right)
+        {
+            if(left == null)
+            {
+                throw new TypeError("undefined method `*' for nil:NilClass");
+            }
+            var result = new Array();
+            for(var i = 0; i < right.Value; i++)
+            {
+                result.list.AddRange(left);
+            }
+            return result;
+        }
+
+        public static string operator *(Array left, string right) => left.Join(right);
+
+        public static Array operator -(Array left, Array right)
+        {
+            if (left == null || right == null)
+            {
+                throw new TypeError("no implicit conversion of nil into Array");
+            }
+            var result = new Array(left.list);
+            result.list.RemoveAll(item => right.Contains(item));
+            return result;
+        }
 
         public static explicit operator Array(iObject[] objects) => new Array(objects);
     }
--- UnitTests/ArrayTests.cs
+++ UnitTests/ArrayTests.cs
@@ -9,35 +9,325 @@ namespace Mint.UnitTests
         [Test]
         public void TestCreate()
         {
+            var fixnum3 = new Fixnum(3);
             Assert.That(new Array(), Is.Not.Null);
             Assert.That(new Array(new iObject[0]), Is.Not.Null);
+
+            var array = new Array((int) fixnum3.Value, new Hash());
+            Assert.That(array, Is.Not.Null);
+            Assert.That(array.Count, Is.EqualTo((int) fixnum3.Value));
+
+            array = new Array((int) fixnum3.Value);
+            Assert.That(array, Is.Not.Null);
+            Assert.That(array.Count, Is.EqualTo((int) fixnum3.Value));
         }
 
         [Test]
+        public void TestDefaultValue()
+        {
+            var fixnum3 = new Fixnum(3);
+            var hash = new Hash();
+            var array = new Array(fixnum3, hash);
+            Assert.That(array[0], Is.EqualTo(hash));
+            Assert.That(array[1], Is.EqualTo(hash));
+            Assert.That(array[2], Is.EqualTo(hash));
+        }
+
+        [Test] //count length size
         public void TestCount()
         {
+            // [1, "string"].count = 2
             var emptyArray = new Array();
             Assert.That(emptyArray.Count, Is.EqualTo(0));
             var array = new Array(new Fixnum(1), new String("string"));
             Assert.That(array.Count, Is.EqualTo(2));
+
+            // TODO [1, "string"].count(1) = 1
+            // TODO [1, 2, 3, 5].count { |x| x % 2 == 0 } = 3
+            Assert.Fail("Test Not Implemented Yet");
         }
 
-        [Test]
+        // TODO
+        // try_convert
+        // <=>
+        // ==
+        // [start, length]=
+        // [range]=
+        // any?
+        // assoc
+        // at
+        // bsearch
+        // collect collect!
+        // combination
+        // concat
+        // cycle
+        // delete_at
+        // delete_if
+        // drop
+        // drop_while
+        // each
+        // each_index
+        // empty?
+        // eql?
+        // fetch
+        // fill
+        // find_index
+        // flatten flatten!
+        // frozen?
+        // hash
+        // include?
+        // index
+        // initialize_copy
+        // insert
+        // keep_if
+        // map map!
+        // pack
+        // permutation
+        // pop
+        // product
+        // rassoc
+        // reject reject!
+        // repeated_combination
+        // repeated_permutation
+        // reverse_each
+        // rindex
+        // rotate rotate!
+        // sample
+        // select select!
+        // shift
+        // shuffle shuffle!
+        // sort sort!
+        // sort_by!
+        // take
+        // take_while
+        // to_a
+        // to_ary
+        // to_h
+        // transpose
+        // uniq uniq!
+        // unshift
+        // values_at
+        // zip
+        // |
+
+        [Test] // &
+        public void TestAnd()
+        {
+            // [1, 1, 3, 5] & [1, 2, 3] = [1, 3]
+            var fixnum1 = new Fixnum(1);
+            var fixnum3 = new Fixnum(3);
+            var array = new Array(fixnum1, fixnum1, fixnum3, new Fixnum(5));
+            var otherArray = new Array(fixnum1, new Fixnum(2), fixnum3);
+            var expectedResult = new Array(fixnum1, fixnum3);
+            var result = array.AndAlso(otherArray);
+            Assert.That(array.Count, Is.EqualTo(2));
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test] // * join
+        public void TestMultiply()
+        {
+            // [1, 2, 3] * 3 = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+            var fixnum1 = new Fixnum(1);
+            var fixnum2 = new Fixnum(2);
+            var fixnum3 = new Fixnum(3);
+            var array = new Array(fixnum1, fixnum2, fixnum3);
+            var result = array * fixnum3;
+            var expectedResult = new Array(fixnum1, fixnum2, fixnum3, fixnum1, fixnum2, fixnum3, fixnum1, fixnum2, fixnum3);
+            Assert.That(array.Count, Is.EqualTo(9));
+            Assert.That(result, Is.EqualTo(expectedResult));
+            
+            // [1, 2, 3] * "," = "1,2,3"
+            var result2 = array * ",";
+            Assert.That(result2, Is.EqualTo(new String("1,2,3")));
+
+            result2 = array.Join();
+            Assert.That(result2, Is.EqualTo(new String("123")));
+
+            // [1, 2, 3].join(",") = "1,2,3"
+            result2 = array.Join(",");
+            Assert.That(result2, Is.EqualTo(new String("1,2,3")));
+
+            // TODO move to fixnum 1 * [1, 2, 3] = TypeError
+            // Assert.Throws<TypeError>(() => { var a = fixnum1 * array; });
+
+            // [1, 2, 3] *= 3
+            array *= fixnum3;
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+
+        [Test] // +
+        public void TestPlus()
+        {
+            // [1, 2, 3] + [4, 5] = [1, 2, 3, 4, 5]
+            var fixnum1 = new Fixnum(1);
+            var fixnum2 = new Fixnum(2);
+            var fixnum3 = new Fixnum(3);
+            var fixnum4 = new Fixnum(4);
+            var fixnum5 = new Fixnum(5);
+            var array = new Array(fixnum1, fixnum2, fixnum3);
+            var otherArray = new Array(fixnum4, fixnum5);
+            var expectedResult = new Array(fixnum1, fixnum2, fixnum3, fixnum4, fixnum5);
+            var result = array + otherArray;
+            Assert.That(result, Is.EqualTo(expectedResult));
+
+            // [4, 5] + [1, 2, 3] = [4, 5, 1, 2, 3]
+            expectedResult = new Array(fixnum4, fixnum5, fixnum1, fixnum2, fixnum3);
+            result = otherArray + array;
+            Assert.That(result, Is.EqualTo(expectedResult));
+
+            
+            Assert.Throws<TypeError>(() => { var a = array + (Array) null; }); // [1, 2, 3] + nil
+            Assert.Throws<TypeError>(() => { var a = (Array) null + array; }); // nil + [1, 2, 3]
+        }
+
+        [Test] // -
+        public void TestMinus()
+        {
+            // [ 1, 1, 2, 2, 3, 3, 4, 5 ] - [ 1, 2, 4 ]  #=>  [ 3, 3, 5 ]
+            var fixnum1 = new Fixnum(1);
+            var fixnum2 = new Fixnum(2);
+            var fixnum3 = new Fixnum(3);
+            var fixnum4 = new Fixnum(4);
+            var fixnum5 = new Fixnum(5);
+            var array = new Array(fixnum1, fixnum1, fixnum2, fixnum2, fixnum3, fixnum3, fixnum4, fixnum5);
+            var otherArray = new Array(fixnum1, fixnum2, fixnum4);
+            var expectedResult = new Array(fixnum3, fixnum3, fixnum5);
+            var result = array - otherArray;
+            Assert.That(result, Is.EqualTo(expectedResult));
+
+            Assert.Throws<TypeError>(() => { var a = array + (Array) null; }); // [1, 2, 3] - nil
+            Assert.Throws<TypeError>(() => { var a = (Array) null + array; });  // nil - [1, 2, 3]
+        }
+
+        [Test] // <<
+        public void TestAdd()
+        {
+            // [ 1, 2 ] << "c" << "d" << [ 3, 4 ] => [ 1, 2, "c", "d", [ 3, 4 ] ]
+            var fixnum1 = new Fixnum(1);
+            var fixnum2 = new Fixnum(2);
+            var fixnum3 = new Fixnum(3);
+            var fixnum4 = new Fixnum(4);
+            var c = new String("c");
+            var d = new String("d");
+            var array1 = new Array(fixnum1, fixnum2);
+            var array2 = new Array(fixnum3, fixnum4);
+            var expected = new Array(fixnum1, fixnum2, c, d, array2);
+            Assert.That(array1.Add(c).Add(d).Add(array2), Is.EqualTo(expected));
+
+            // TODO Assert.Throws<ArgumentError> (() =>  `a.<<*[1, 2, 3]` )
+        }
+
+        [Test] // TODO <=>
+        public void TestCompareTo()
+        {
+            // [ "a", "a", "c" ] <=> [ "a", "b", "c" ]   #=> -1
+            var a = new String("a");
+            var b = new String("b");
+            var c = new String("c");
+            var fixnum1 = new Fixnum(1);
+            var fixnum2 = new Fixnum(2);
+            var fixnum3 = new Fixnum(3);
+            var fixnum4 = new Fixnum(4);
+            var fixnum5 = new Fixnum(5);
+            var fixnum6 = new Fixnum(6);
+            var symbol = new Symbol("two");
+            var array = new Array(a, a, c);
+            var otherArray = new Array(a, b, c);
+            // Assert.That(array.CompareTo(otherArray), Is.EqualTo(-1));
+
+            //[ 1, 2, 3, 4, 5, 6 ] <=> [ 1, 2 ] => +1
+            array = new Array(fixnum1, fixnum2, fixnum3, fixnum4, fixnum5, fixnum6);
+            otherArray = new Array(fixnum1, fixnum2);
+            // Assert.That(array.CompareTo(otherArray), Is.EqualTo(1));
+
+            // [ 1, 2 ] <=> [ 1, :two ] => nil
+            // Assert.That(array.CompareTo(otherArray), Is.EqualTo(new NilClass()));
+            Assert.Fail("Test Not Implemented Yet");
+        }
+
+        [Test] // TODO ==
+        public void TestEqual()
+        {
+            // [ "a", "c" ]    == [ "a", "c", 7 ]     #=> false
+            var a = new String("a");
+            var c = new String("c");
+            var d = new String("d");
+            var f = new String("f");
+            var fixnum7 = new Fixnum(7);
+            var array = new Array(a, c);
+            var otherArray = new Array(a, c, fixnum7);
+            // Assert.That(array == otherArray, Is.False);
+            // [ "a", "c", 7 ] == [ "a", "c", 7 ]     #=> true
+            array = new Array(a, c, fixnum7);
+            // Assert.That(array == otherArray, Is.True);
+
+            // [ "a", "c", 7 ] == [ "a", "d", "f" ]   #=> false
+            otherArray = new Array(a, d, f);
+            // Assert.That(array == otherArray, Is.False);
+            Assert.Fail("Test Not Implemented Yet");
+        }
+
+        [Test] // clear
+        public void TestClear()
+        {
+            var array = new Array(new Fixnum(1), new String("string"));
+            array.Clear();
+            Assert.That(array.Count, Is.EqualTo(0));
+        }
+
+        [Test] // compact compact!
+        public void TestCompact()
+        {
+            var nil = new NilClass();
+            var fixnum0 = new Fixnum(0);
+            var array = new Array(nil, nil);
+            var newArray = array.Compact();
+            Assert.That(newArray.Count, Is.EqualTo(0));
+            Assert.That(array.Count, Is.EqualTo(2));
+            array.CompactSelf();
+            Assert.That(array.Count, Is.EqualTo(0));
+        }
+
+        [Test] // delete TODO
+        public void TestDelete()
+        {
+
+        }
+
+        [Test] // [] [start, length] [range] first last slice slice!
         public void TestIndexerGetter()
         {
+            var fixnum0 = new Fixnum(0);
+            var str = new String("string");
+            var nil = new NilClass();
             var emptyArray = new Array();
-            Assert.That(emptyArray[0], Is.EqualTo(new NilClass()));
-            var array = new Array(new Fixnum(1), new String("string"));
-            Assert.That(array[0], Is.EqualTo(new Fixnum(1)));
-            Assert.That(array[1], Is.EqualTo(new String("string")));
-            Assert.That(array[2], Is.EqualTo(new NilClass()));
-            Assert.That(array[-2], Is.EqualTo(new Fixnum(1)));
-            Assert.That(array[-1], Is.EqualTo(new String("string")));
-            Assert.That(array[-3], Is.EqualTo(new NilClass()));
-            Assert.Throws<TypeError>(() => { var a = array[new Symbol("symbol")]; });
+            Assert.That(emptyArray[0], Is.EqualTo(nil));
+            var array = new Array(fixnum0, str);         // a = [0, "string"]
+            
+            // []
+            Assert.That(array[0], Is.EqualTo(fixnum0));  //  a[0] => 0
+            Assert.That(array[1], Is.EqualTo(str));      //  a[1] => "string"
+            Assert.That(array[2], Is.EqualTo(nil));      //  a[2] => nil
+
+            Assert.That(array[-2], Is.EqualTo(fixnum0)); // a[-2] => 0
+            Assert.That(array[-1], Is.EqualTo(str));     // a[-1] => "string"
+            Assert.That(array[-3], Is.EqualTo(nil));     // a[-3] => "string"
+
+            // TODO
+            // a[0, 3]  => [0, "string"]
+            // a[-2, 1] => [0]
+            // a[0..5]  => [0, "string"]
+            // a[0...1] => [0]
+            Assert.That(array.First, Is.EqualTo(fixnum0)); // a.first => 0
+            Assert.That(array.Last, Is.EqualTo(str));      // a.last  => "string"
+
+            // TODO
+            // slice!
+            Assert.Fail("Test Not Implemented Yet");
         }
 
-        [Test]
+        [Test] // []= [start, length]= [range]=
         public void TestIndexerSetter()
         {
             var fixnum1 = new Fixnum(1);
@@ -69,10 +359,9 @@ namespace Mint.UnitTests
             Assert.That(array[-2], Is.EqualTo(nil));
 
             Assert.Throws<IndexError>(() => { emptyArray[-3] = fixnum1; });
-            Assert.Throws<TypeError>(() => { emptyArray[nil] = fixnum1; });
         }
 
-        [Test]
+        [Test] // push
         public void TestPush()
         {
             var fixnum1 = new Fixnum(1);
@@ -86,9 +375,44 @@ namespace Mint.UnitTests
             Assert.That(array.Push(fixnum1), Is.EqualTo(new Array(fixnum1, str, nil, fixnum1)));
         }
 
-        [Test]
+        [Test] // replace
+        public void TestReplace()
+        {
+            // [1, 2, 3].replace(['a', 'b', 'c']) = ['a', 'b', 'c']
+            var a = new String("a");
+            var b = new String("b");
+            var c = new String("c");
+            var fixnum1 = new Fixnum(1);
+            var fixnum2 = new Fixnum(2);
+            var fixnum3 = new Fixnum(3);
+            var array = new Array(fixnum1, fixnum2, fixnum3);
+            var otherArray = new Array(a, b, c);
+            array.Replace(otherArray);
+            Assert.That(array, Is.EqualTo(otherArray));
+        }
+
+        [Test] // reverse reverse!
+        public void TestReverse()
+        {
+            // [1, 2, 3].reverse = [3, 2, 1]
+            var a = new String("a");
+            var b = new String("b");
+            var c = new String("c");
+            var fixnum1 = new Fixnum(1);
+            var fixnum2 = new Fixnum(2);
+            var fixnum3 = new Fixnum(3);
+            var array = new Array(fixnum1, fixnum2, fixnum3);
+            var expectedResult = new Array(fixnum3, fixnum2, fixnum1);
+            var result = array.Reverse();
+            Assert.That(result, Is.EqualTo(expectedResult));
+            array.ReverseSelf();
+            Assert.That(array, Is.EqualTo(expectedResult));
+        }
+
+        [Test] // to_string
         public void TestToString()
         {
+            // [1, "string", nil].to_s = "[1, \"string\", nil]"
             var fixnum1 = new Fixnum(1);
             var str = new String("string");
             var nil = new NilClass();
@@ -98,6 +422,21 @@ namespace Mint.UnitTests
 
             var array = new Array(fixnum1, str, nil);
             Assert.That(array.ToString, Is.EqualTo("[1, \"string\", nil]"));
+        }
+
+        [Test] // inspet
+        public void TestInspect()
+        {
+            // [1, "string", nil].to_s = "[1, \"string\", nil]"
+            var fixnum1 = new Fixnum(1);
+            var str = new String("string");
+            var nil = new NilClass();
+
+            var emptyArray = new Array();
+            Assert.That(emptyArray.Inspect, Is.EqualTo("[]"));
+
+            var array = new Array(fixnum1, str, nil);
+            Assert.That(array.Inspect, Is.EqualTo("[1, \"string\", nil]"));
         }
     }
 }
