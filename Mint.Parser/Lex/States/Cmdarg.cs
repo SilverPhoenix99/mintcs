6f5ec07da3b25cd6303cf8985110ee5290c97ad3
Silver Phoenix <silver.phoenix99@gmail.com>
2016-06-09 12:54:33 +0100

    Continued centralization of lexer rules. Cmdarg is now a subtype of Arg, without a need for its own Ragel state machine. FnameFitem is now a subtype of Fname, with a single additional rule.

---
+++ Mint.Parser/Lex/States/Cmdarg.cs
@@ -0,0 +1,31 @@
+using Mint.Parse;
+using static Mint.Parse.TokenType;
+
+namespace Mint.Lex.States
+{
+    internal class Cmdarg : Arg
+    {
+        public Cmdarg(Lexer lexer) : base(lexer)
+        { }
+
+        protected override void EmitDoToken()
+        {
+            var tokenType = kDO;
+
+            if(Lexer.LeftParenCounter > 0 && Lexer.LeftParenCounter == Lexer.ParenNest)
+            {
+                Lexer.LeftParenCounter = 0;
+                Lexer.ParenNest--;
+                tokenType = kDO_LAMBDA;
+            }
+            else if(Lexer.Cond.Peek)
+            {
+                tokenType = kDO_COND;
+            }
+
+            Lexer.EmitToken(tokenType, ts, te);
+            Lexer.CurrentState = Lexer.BegState;
+            Lexer.CommandStart = true;
+        }
+    }
+}
