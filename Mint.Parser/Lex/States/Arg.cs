2c5e74a53cd2d6029a6226a73879937a31e7179a
Silver Phoenix <silver.phoenix99@gmail.com>
2016-06-11 13:02:17 +0100

    Unified Arg and ArgLabeled common rules into ArgBase.

--- Mint.Parser/Lex/States/Arg.cs
+++ Mint.Parser/Lex/States/Arg.cs
@@ -3,57 +3,12 @@ using static Mint.Parse.TokenType;
 
 namespace Mint.Lex.States
 {
-    internal partial class Arg : Shared
+    internal partial class Arg : ArgBase
     {
         protected override bool CanLabel => true;
 
         public Arg(Lexer lexer) : base(lexer)
         { }
-
-        protected override void EmitIdentifierToken()
-        {
-            var token = Lexer.EmitToken(tIDENTIFIER, ts, te);
-            var isLocalVar = Lexer.IsVariableDefined(token.Value);
-            if(isLocalVar)
-            {
-                Lexer.CurrentState = Lexer.EndState;
-                Lexer.CanLabel = true;
-            }
-            else
-            {
-                Lexer.CurrentState = Lexer.CommandStart ? Lexer.CmdargState : Lexer.ArgState;
-            }
-        }
-
-        protected override void EmitFidToken()
-        {
-            Lexer.EmitToken(tFID, ts, te - 1);
-            Lexer.CurrentState = Lexer.CommandStart ? Lexer.CmdargState : Lexer.ArgState;
-        }
-
-        protected override void EmitDoToken()
-        {
-            var tokenType = kDO;
-
-            if(Lexer.LeftParenCounter > 0 && Lexer.LeftParenCounter == Lexer.ParenNest)
-            {
-                Lexer.LeftParenCounter = 0;
-                Lexer.ParenNest--;
-                tokenType = kDO_LAMBDA;
-            }
-            else if(Lexer.Cond.Peek)
-            {
-                tokenType = kDO_COND;
-            }
-            else if(Lexer.Cmdarg.Peek)
-            {
-                tokenType = kDO_BLOCK;
-            }
-
-            Lexer.EmitToken(tokenType, ts, te);
-            Lexer.CurrentState = Lexer.BegState;
-            Lexer.CommandStart = true;
-        }
 
         protected override void EmitLeftBrace()
         {
--- Mint.Parser/Lex/States/ArgLabeled.cs
+++ Mint.Parser/Lex/States/ArgLabeled.cs
@@ -3,7 +3,7 @@ using static Mint.Parse.TokenType;
 
 namespace Mint.Lex.States
 {
-    internal partial class ArgLabeled : Shared
+    internal partial class ArgLabeled : ArgBase
     {
         protected override TokenType DoubleStarTokenType => kDSTAR;
         protected override TokenType StarTokenType => kSTAR;
@@ -17,51 +17,6 @@ namespace Mint.Lex.States
 
         public ArgLabeled(Lexer lexer) : base(lexer)
         { }
-
-        protected override void EmitIdentifierToken()
-        {
-            var token = Lexer.EmitToken(tIDENTIFIER, ts, te);
-            var isLocalVar = Lexer.IsVariableDefined(token.Value);
-            if(isLocalVar)
-            {
-                Lexer.CurrentState = Lexer.EndState;
-                Lexer.CanLabel = true;
-            }
-            else
-            {
-                Lexer.CurrentState = Lexer.CommandStart ? Lexer.CmdargState : Lexer.ArgState;
-            }
-        }
-
-        protected override void EmitFidToken()
-        {
-            Lexer.EmitToken(tFID, ts, te - 1);
-            Lexer.CurrentState = Lexer.CommandStart ? Lexer.CmdargState : Lexer.ArgState;
-        }
-
-        protected override void EmitDoToken()
-        {
-            var tokenType = kDO;
-
-            if(Lexer.LeftParenCounter > 0 && Lexer.LeftParenCounter == Lexer.ParenNest)
-            {
-                Lexer.LeftParenCounter = 0;
-                Lexer.ParenNest--;
-                tokenType = kDO_LAMBDA;
-            }
-            else if(Lexer.Cond.Peek)
-            {
-                tokenType = kDO_COND;
-            }
-            else if(Lexer.Cmdarg.Peek)
-            {
-                tokenType = kDO_BLOCK;
-            }
-
-            Lexer.EmitToken(tokenType, ts, te);
-            Lexer.CurrentState = Lexer.BegState;
-            Lexer.CommandStart = true;
-        }
 
         protected override void EmitModifierKeywordToken(TokenType type)
         {
