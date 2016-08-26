74bc134e95f609a0a491af6515c4fe68dba2c136
Silver Phoenix <silver.phoenix99@gmail.com>
2016-08-26 16:39:57 +0100

    Commend on ParameterKind enum.

--- Mint.Reflection/Parameters/ParameterKind.cs
+++ Mint.Reflection/Parameters/ParameterKind.cs
@@ -2,13 +2,14 @@
 {
     public enum ParameterKind
     {
-        Required,     // a
-        Optional,     // a = nil
-        Rest,         // *a
-        Block,        // &a
-        KeyRequired,  // a:
-        KeyOptional,  // a: nil
-        KeyRest,      // **a
-        Parallel,     // (a, b)
+        //                          Min  Max
+        Required,     // a           +1   +1
+        Optional,     // a = nil     +0   +1
+        Rest,         // *a          +0  Inf
+        Block,        // &a          +0   +1
+        KeyRequired,  // a:          +1   +1
+        KeyOptional,  // a: nil      +0   +1
+        KeyRest,      // **a         +0  Inf
+        Parallel,     // (a, b)      +1   +1
     }
 }
