6091dff02296cf5ba877ba4562769bc19b68eb72
Silver Phoenix <silver.phoenix99@gmail.com>
2016-10-18 20:53:55 +0100

    Added Scopes.

---
+++ Mint.Compiler/Compilation/Scopes/Scope.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Mint.Compilation.Scopes
+{
+    public interface Scope
+    {
+        Scope Parent { get; }
+
+        ParameterExpression Nesting { get; }
+
+        LabelTarget BreakLabel { get; }
+
+        //LabelTarget NextLabel { get; }
+
+        //LabelTarget RedoLabel { get; }
+
+        //LabelTarget RetryLabel { get; }
+    }
+
+    public class TopLevelScope : Scope
+    {
+        public Scope Parent => this;
+
+        public ParameterExpression Nesting { get; }
+
+        public LabelTarget BreakLabel { get { throw new SyntaxError("Invalid break"); } }
+
+        public LabelTarget NextLabel { get { throw new SyntaxError("Invalid next"); } }
+
+        public LabelTarget RedoLabel { get { throw new SyntaxError("Invalid redo"); } }
+
+        public LabelTarget RetryLabel { get { throw new SyntaxError("Invalid retry"); } }
+
+        public TopLevelScope()
+        {
+            Nesting = Expression.Variable(typeof(IList<Module>), "nesting");
+        }
+    }
+
+    public class MethodScope : Scope
+    {
+        public Scope Parent { get; }
+
+        public ParameterExpression Nesting => Parent.Nesting;
+
+        public LabelTarget BreakLabel { get { throw new SyntaxError("Invalid break"); } }
+
+        public LabelTarget NextLabel { get { throw new SyntaxError("Invalid next"); } }
+
+        public LabelTarget RedoLabel { get { throw new SyntaxError("Invalid redo"); } }
+
+        public LabelTarget RetryLabel { get { throw new SyntaxError("Invalid retry"); } }
+
+        public MethodScope(Scope parent)
+        {
+            Parent = parent;
+        }
+    }
+
+    public class BlockScope : MethodScope
+    {
+        public BlockScope(Scope parent) : base(parent)
+        { }
+    }
+
+    public class WhileScope : MethodScope
+    {
+        public WhileScope(Scope parent) : base(parent)
+        { }
+    }
+
+    public class ForScope : WhileScope
+    {
+        public ForScope(Scope parent) : base(parent)
+        { }
+    }
+
+    public class ModuleScope : Scope
+    {
+        public Scope Parent { get; }
+
+        public ParameterExpression Nesting { get; }
+
+        public LabelTarget BreakLabel { get { throw new System.NotImplementedException(); } }
+
+        public ModuleScope(Scope parent)
+        {
+            Parent = parent;
+            Nesting = Expression.Variable(typeof(IList<Module>), "nesting");
+        }
+    }
+
+    public class ClassScope : ModuleScope
+    {
+        public ClassScope(Scope parent) : base(parent)
+        { }
+    }
+}
--- Mint.Parser/SyntaxError.cs
+++ Mint.Parser/SyntaxError.cs
@@ -4,15 +4,14 @@ namespace Mint
 {
     public class SyntaxError : Exception
     {
-        public SyntaxError()
+        public SyntaxError(string message) : base(message)
         { }
 
-        public SyntaxError(string filename, int line, string message)
-            : base($"{filename}:{line}: {message}")
+        public SyntaxError(string message, Exception innerException) : base(message, innerException)
         { }
 
-        public SyntaxError(string filename, int line, string message, Exception innerException)
-            : base($"{filename}:{line}: {message}", innerException)
+        public SyntaxError(string filename, int line, string message, Exception innerException = null)
+            : this($"{filename}:{line}: {message}", innerException)
         { }
     }
 }
